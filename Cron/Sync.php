cc8ebebc09eb75954675b5968e7d9a2e87b3b971
Vitaly Zilnik <vitaly.zilnik@gmail.com>
2017-05-11 00:32:51 +0300

    Cache usage removed.

--- Cron/Sync.php
+++ Cron/Sync.php
@@ -2,7 +2,6 @@
 
 namespace Strategery\Stockbase\Cron;
 
-use Magento\Framework\App\CacheInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Psr\Log\LoggerInterface;
 use Strategery\Stockbase\Api\Client\StockbaseClientFactory;
@@ -11,8 +10,6 @@ use Strategery\Stockbase\Model\ResourceModel\StockItem as StockItemResource;
 
 class Sync
 {
-    const LAST_MUTATION_TIMESTAMP_CACHE_KEY = 'stockbase_stock_last_mutation_timestamp';
-    
     /**
      * @var LoggerInterface
      */
@@ -26,10 +23,6 @@ class Sync
      */
     private $objectManager;
     /**
-     * @var CacheInterface
-     */
-    private $cache;
-    /**
      * @var StockbaseConfiguration
      */
     private $config;
@@ -37,7 +30,6 @@ class Sync
     public function __construct(
         LoggerInterface $logger,
         ObjectManagerInterface $objectManager,
-        CacheInterface $cache,
         StockbaseClientFactory $stockbaseClientFactory,
         StockbaseConfiguration $config
     )
@@ -45,7 +37,6 @@ class Sync
         $this->logger = $logger;
         $this->stockbaseClientFactory = $stockbaseClientFactory;
         $this->objectManager = $objectManager;
-        $this->cache = $cache;
         $this->config = $config;
     }
 
@@ -61,59 +52,19 @@ class Sync
         /** @var StockItemResource $stockItemResource */
         $stockItemResource = $this->objectManager->create(StockItemResource::class);
         
-        $lastSyncDate = $this->cache->load(self::LAST_MUTATION_TIMESTAMP_CACHE_KEY);
-        if ($lastSyncDate !== false) {
-            $lastSyncDate = \DateTime::createFromFormat('U', $lastSyncDate);
-        } else {
-            $lastSyncDate = null;
-        }
+        $lastModifiedDate = $stockItemResource->getLastModifiedItemDate();
 
         $this->logger->info(sprintf(
             "Downloading Stockbase stock data since %s...",
-            $lastSyncDate !== null ? $lastSyncDate->format('Y-m-d H:i:s') : 'the beginning'
+            $lastModifiedDate !== null ? $lastModifiedDate->format('Y-m-d H:i:s') : 'the beginning'
         ));
-        
-        $stock = $client->getStock($lastSyncDate);
+        $stock = $client->getStock($lastModifiedDate);
 
         $this->logger->info("Updating local index...");
-        $data = [];
-        $latestTimestamp = null;
-        $count = 0;
-        foreach ($stock->Groups as $group) {
-            foreach ($group->Items as $item) {
-                $count++;
-                $data[] = [
-                    'ean' => $item->EAN,
-                    'brand' => isset($group->Brand) ? $group->Brand : null,
-                    'code' => isset($group->Code) ? $group->Code : null,
-                    'supplier_code' => isset($group->SupplierCode) ? $group->SupplierCode : null,
-                    'supplier_gln' => !empty($group->SupplierGLN) ? $group->SupplierGLN : null,
-                    'amount' => $item->Amount,
-                    'noos' => $item->NOOS,
-                    'timestamp' => date('Y-m-d H:i:s', $item->Timestamp),
-                ];
-                //var_dump($data);
-                if (count($data) >= 100) {
-                    $stockItemResource->bulkUpdate($data);
-                    $data = [];
-                }
-                if ($item->Timestamp > $latestTimestamp) {
-                    $latestTimestamp = (int)$item->Timestamp;
-                }
-            }
-        }
-        if (count($data) > 0) {
-            $stockItemResource->bulkUpdate($data);
-        }
+        $total = $stockItemResource->updateFromStockObject($stock);
         
-        if ($latestTimestamp !== null) {
-            $this->logger->info(sprintf(
-                "%s Stockbase items updated. Last stock item mutation time: %s",
-                $count,
-                date('Y-m-d H:i:s', $latestTimestamp)
-            ));
-            $this->cache->save($latestTimestamp, self::LAST_MUTATION_TIMESTAMP_CACHE_KEY);
-            
+        if ($total > 0) {
+            $this->logger->info("{$total} Stockbase items updated.");
         } else {
             $this->logger->info("No new updated was found.");
         }
--- Model/ResourceModel/StockItem.php
+++ Model/ResourceModel/StockItem.php
@@ -6,6 +6,8 @@ namespace Strategery\Stockbase\Model\ResourceModel;
 
 class StockItem extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
+    const BULK_INSERT_CHUNK_SIZE = 100;
+    
     /**
      * @var \Magento\Framework\EntityManager\EntityManager
      */
@@ -16,7 +18,61 @@ class StockItem extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
         $this->_init('stockbase_stock', 'ean');
     }
 
-    public function bulkUpdate(array $data)
+    /**
+     * Gets the modification date of the last modified item in the stock.
+     * 
+     * @return \DateTime|null
+     */
+    public function getLastModifiedItemDate()
+    {
+        $connection = $this->getConnection();
+
+        $query = $connection->select()
+            ->from($this->getMainTable(), 'timestamp')
+            ->order('timestamp DESC')
+            ->limit(1);
+
+        $result = $connection->fetchCol($query);
+        
+        return !empty($result[0]) ? new \DateTime($result[0]) : null;
+    }
+
+    /**
+     * Updates the local stock based on given Stockbase API response.
+     * 
+     * @param \stdClass $stock
+     * @return int
+     */
+    public function updateFromStockObject(\stdClass $stock)
+    {
+        $data = [];
+        $total = 0;
+        foreach ($stock->Groups as $group) {
+            foreach ($group->Items as $item) {
+                $total++;
+                $data[] = [
+                    'ean' => $item->EAN,
+                    'brand' => !empty($group->Brand) ? $group->Brand : null,
+                    'code' => !empty($group->Code) ? $group->Code : null,
+                    'supplier_code' => !empty($group->SupplierCode) ? $group->SupplierCode : null,
+                    'supplier_gln' => !empty($group->SupplierGLN) ? $group->SupplierGLN : null,
+                    'amount' => $item->Amount,
+                    'noos' => $item->NOOS,
+                    'timestamp' => date('Y-m-d H:i:s', $item->Timestamp),
+                ];
+                if (count($data) >= self::BULK_INSERT_CHUNK_SIZE) {
+                    $this->bulkUpdate($data);
+                    $data = [];
+                }
+            }
+        }
+        if (count($data) > 0) {
+            $this->bulkUpdate($data);
+        }
+        return $total;
+    }
+
+    protected function bulkUpdate(array $data)
     {
         $connection = $this->getConnection();
         
