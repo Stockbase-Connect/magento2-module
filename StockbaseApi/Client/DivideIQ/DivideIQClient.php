9bd0cf186dcc7172f6a47d9a2cc93ff69332e0ee
Vitaly Zilnik <vitaly.zilnik@gmail.com>
2017-06-07 18:29:45 +0300

    Composer dependencies updated, old 3rd-party bug workaround removed.

--- StockbaseApi/Client/DivideIQ/DivideIQClient.php
+++ StockbaseApi/Client/DivideIQ/DivideIQClient.php
@@ -32,56 +32,6 @@ class DivideIQClient extends DivideIQ
         $this->client->setDefaultOption('timeout', $timeout);
     }
 
-
-    /**
-     * {@inheritdoc}
-     */
-    public function request($serviceName, $payload = [], $method = 'GET')
-    {
-        // Setup the connection.
-        $this->setup();
-
-        $path = $this->settings->getPath($serviceName);
-
-        switch ($method) {
-            case 'GET':
-                // Perform the request with the payload as a query string.
-                $response = $this->client->get($path, [
-                    'headers' => ['Authentication' => $this->accessToken->getToken()],
-                    'query' => $payload,
-                ]);
-
-                break;
-            case 'POST':
-                $response = $this->client->post($path, [
-                    'headers' => ['Authentication' => $this->accessToken->getToken()],
-                    'json' => $payload,
-                ]);
-
-                break;
-        }
-
-        // Parse the response body.
-        $body = $response->json(['object' => true])->{'nl.divide.iq'};
-
-        // Check if the settings object is outdated. If so, unset it.
-        if ($this->settings->isOutdated($body->{'settings_updated'})) {
-            unset($this->settings);
-        }
-
-        // Check if there was an error.
-        if (!isset($body->response->content)) {
-            $message = $body->response->answer;
-            $message .= isset($body->response->message) ? ": {$body->response->message}" : '';
-
-            // Throw an exception with the error description from the service.
-            throw new \Exception($message);
-        }
-
-        // Return only the response content, without the metadata.
-        return $body->response->content;
-    }
-
     /**
      * {@inheritdoc}
      */
