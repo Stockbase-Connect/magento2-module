5f91b1f7b6dd42b3e7a0b6853abc6ab4c166aba0
Gabriel Somoza <gabriel@strategery.io>
2017-09-08 12:03:15 +0200

    Updated main README.md and integrated it to the main docs

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -e CSS_BOOTSWATCH=flatly -e CSS_PRISM=coy -v $(pwd):/app sandrokeil/bookdown book/bookdown.json @a
+docker run -it --rm -e CSS_BOOTSWATCH=flatly -e CSS_PRISM=coy -v "$(pwd)/..":/app sandrokeil/bookdown docs/book/bookdown.json @a
